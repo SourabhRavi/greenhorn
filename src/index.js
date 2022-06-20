ee8081962f43c6c9f3a18d027b5a4779546360b5
SourabhRavi <skravi0657@gmail.com>
2022-06-20 21:19:01 +0530

    u

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <div>
+        <h1>My Favourite Foods</h1>
+        <ul>
+            <li>Pizza</li>
+            <li>Burger</li>
+            <li>Pasta</li>
+        </ul>
+    </div>
+);
